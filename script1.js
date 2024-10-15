7b485417d6cb438fd66344e446a4d321ea6094a1
naveen-r25 <kgpnaveen2@gmail.com>
2024-10-15 11:58:59 +0530

    css

---
+++ script1.js
@@ -0,0 +1,9 @@
+console.log("hi");
+var x = 5;
+if (x > 10) {  
+  console.log("x is greater than 10");
+} else if (x > 0) {
+  console.log("x is greater than 0 but less than or equal to 10");
+} else {
+  console.log("x is less than or equal to 0");
+}
